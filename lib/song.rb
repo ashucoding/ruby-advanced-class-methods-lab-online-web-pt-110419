75245da4618b6dd1a51244054d41b4d278b9dc99
Shana Wiltshire <shanawiltshire@gmail.com>
2019-12-01 21:46:12 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,9 @@ class Song
     self.class.all << self
   end
 
-  def self.create
+def self.create
     song = self.new
+    @@all << song
     song
   end
   
@@ -35,3 +36,34 @@ class Song
   def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create_by_name(name)
   end
+
+def self.alphabetical()
+  @@all.sort_by{|x| x.name}
+  end
+  
+  
+  def self.new_from_filename(name)
+    song = self.new 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
+  
+  def self.create_from_filename(name)
+  #class method should not only parse the filename correctly but should also save the song
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song
+    song
+  end
+  
+  def self.destroy_all()
+    @@all.clear
+  end
+
+end
+
+song_1 = Song.find_or_create_by_name("Blank Space")
+song_2 = Song.find_or_create_by_name("Blank Space")
+ 
